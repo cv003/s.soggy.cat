e36346740cfd0025bb68a198dd3cfe3e8e56d812
mat5555 <matcraft552mg@hotmail.com>
2023-08-15 06:18:13 +0200

    make it good, and fix a lot of stuff

--- script.js
+++ script.js
@@ -18,51 +18,78 @@ preload(
     "https://soggy.cat/img/pfp/louis.jpg",
 )
 
-var audio = new Audio("assets/audio/cats.mp3");
 var explode = new Audio("assets/audio/boom.mp3");
+
+//credit to https://stackoverflow.com/a/25938297
+
+var current_player = "a";
+var player_a = document.createElement("audio");
+var player_b = document.createElement("audio");
+
+player_a.src = "assets/audio/cats.ogg";
+player_b.src = player_a.src;
+
+function playMusic(){
+    var player = null;
+
+    if(current_player == "a"){
+        player = player_b;
+        current_player = "b";
+    }
+    else{
+        player = player_a;
+        current_player = "a";
+    }
+
+    player.play();
+
+    setTimeout(playMusic, 61967);
+}
+
 
 function showContainer(){
     const overlay = document.querySelector(".overlay");
     overlay.style.opacity = "0";
     overlay.style.pointerEvents = "none";
-    audio.play();
     explode.play();
     animateTitle();
+    playMusic();
     displayImages();
   
     let explosion = document.getElementById('explode');
     explosion.style.display = 'block';
     setTimeout(function(){explosion.style.display = 'none';}, 750)
-    setTimeout(function(){document.write()}, 62000)
+    //setTimeout(function(){document.write()}, 62000)
 }
 
 function animateTitle() {
       i >= message.length - 1 ? (i = 0) : i++,
         (document.title = message[i]),
-        setTimeout("animateTitle()", 200);
+        setTimeout("animateTitle()", 483.8709676);
 }
 
 var message = [
-  "ssoggycat enterprise ",
-  "soggycat enterprise s",
-  "oggycat enterprise ss",
-  "ggycat enterprise sso",
-  "gycat enterprise ssog",
-  "ycat enterprise ssogg",
-  "cat enterprise ssoggy",
-  "at enterprise ssoggyc",
-  "t enterprise ssoggyca",
-  " enterprise ssoggycat",
-  "enterprise ssoggycat ",
-  "nterprise ssoggycat e",
-  "terprise ssoggycat en",
-  "erprise ssoggycat ent",
-  "rprise ssoggycat ente",
-  "prise ssoggycat enter",
-  "rise ssoggycat enterp",
-  "ise ssoggycat enterpr",
-  "se ssoggycat enterpri",
-  "e ssoggycat enterpris",
+  "|ssoggycat enterprise ",
+  "|soggycat enterprise s",
+  "|oggycat enterprise ss",
+  "|ggycat enterprise sso",
+  "|gycat enterprise ssog",
+  "|ycat enterprise ssogg",
+  "|cat enterprise ssoggy",
+  "|at enterprise ssoggyc",
+  "|t enterprise ssoggyca",
+  "| enterprise ssoggycat",
+  "|enterprise ssoggycat",
+  "|nterprise ssoggycat e",
+  "|terprise ssoggycat en",
+  "|erprise ssoggycat ent",
+  "|rprise ssoggycat ente",
+  "|prise ssoggycat enter",
+  "|rise ssoggycat enterp",
+  "|ise ssoggycat enterpr",
+  "|se ssoggycat enterpri",
+  "|e ssoggycat enterpris",
+  "| ssoggycat enterpris",
 ], i = 0;
 
 function displayImages() {
